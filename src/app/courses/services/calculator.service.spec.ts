5bde6116434f38173c4e788c1dfea24bb7361084
Ryan White <mrwhiterk@gmail.com>
2022-08-23 17:25:34 -0400

    6. Our first jasmine spec

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,12 +1,23 @@
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+
 describe ('CalculatorService', () => { // 1 suite has many specs
   
   it('should add two numbers', () => {
-    pending();
+    const calculator = new CalculatorService(new LoggerService());
+
+    const result = calculator.add(2, 2);
+
+    expect(result).toBe(4);
 
   })
 
   it('should subtract two numbers', () => {
-    fail();
+    const calculator = new CalculatorService(new LoggerService());
+
+    const result = calculator.subtract(2, 2);
+
+    expect(result).toBe(0, "unexpected subtraction result");
     
   })
 })
