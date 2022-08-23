476fcd559a835d0fa08fb3147535a454cade0733
Ryan White <mrwhiterk@gmail.com>
2022-08-23 17:10:15 -0400

    5. quick intro to jasmine

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,12 @@
+describe ('CalculatorService', () => { // 1 suite has many specs
+  
+  it('should add two numbers', () => {
+    pending();
+
+  })
+
+  it('should subtract two numbers', () => {
+    fail();
+    
+  })
+})
