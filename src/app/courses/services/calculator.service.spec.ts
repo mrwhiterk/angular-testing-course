7ca3b0103e415f6f2891336a0acb606f7f04007a
Ryan White <mrwhiterk@gmail.com>
2022-08-25 17:03:20 -0400

    9. Using dependency Injection

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,3 +1,4 @@
+import { TestBed } from "@angular/core/testing";
 import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
 
@@ -9,7 +10,14 @@ describe ('CalculatorService', () => { // 1 suite has many specs
   beforeEach(() => {
     loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
 
-    calculator = new CalculatorService(loggerSpy);
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        { provide: LoggerService, useValue: loggerSpy }
+      ]
+    })
+
+    calculator = TestBed.get(CalculatorService);
   })
   
   it('should add two numbers', () => {
