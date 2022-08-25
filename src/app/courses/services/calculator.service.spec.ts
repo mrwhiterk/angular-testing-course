7231cf303bcc0a074d790b35e27be6b4973c0730
Ryan White <mrwhiterk@gmail.com>
2022-08-25 16:54:07 -0400

    8. Structuring Angular unit tests

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -2,31 +2,36 @@ import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
 
 describe ('CalculatorService', () => { // 1 suite has many specs
+
+  let calculator: CalculatorService,
+      loggerSpy: any;
+
+  beforeEach(() => {
+    loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
+
+    calculator = new CalculatorService(loggerSpy);
+  })
   
   it('should add two numbers', () => {
-    
-    const logger = jasmine.createSpyObj('LoggerService', ["log"]);
 
     // spyOn(logger, 'log'); alternate
-
     // logger.log.and.returnValue();
-
-    const calculator = new CalculatorService(logger);
-
     const result = calculator.add(2, 2);
 
     expect(result).toBe(4);
 
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
 
   })
 
   it('should subtract two numbers', () => {
-    const calculator = new CalculatorService(new LoggerService());
 
     const result = calculator.subtract(2, 2);
 
     expect(result).toBe(0, "unexpected subtraction result");
+
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+
     
   })
 })
