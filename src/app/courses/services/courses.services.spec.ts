4509c3030f7a77f2e30878dd51d1fdcfdc7b3a22
Ryan White <mrwhiterk@gmail.com>
2022-08-29 12:23:20 -0400

    12. Angular HTTP Service testing - Step-by-Step Implementation

--- src/app/courses/services/courses.services.spec.ts
+++ src/app/courses/services/courses.services.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from "@angular/core/testing";
 import { CoursesService } from "./courses.service";
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { COURSES } from "../../../../server/db-data";
 
 describe("CoursesService", () => {
 
-  let courseService: CoursesService;
+  let coursesService: CoursesService,
+      httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -14,10 +16,26 @@ describe("CoursesService", () => {
       ]
     })
 
-    courseService = TestBed.get(CoursesService);
+    coursesService = TestBed.get(CoursesService);
+    httpTestingController = TestBed.get(HttpTestingController);
   })
 
   it('should retrieve all courses', () => {
+    coursesService.findAllCourses()
+      .subscribe(courses => {
+        expect(courses).toBeTruthy('No courses returned');
+        expect(courses.length).toBe(12, "incorrect number of courses");
+
+        const course = courses.find(course => course.id == 12);
+
+        expect(course.titles.description).toBe("Angular Testing Course");
+      })
+
+    const req = httpTestingController.expectOne('/api/courses');
+
+    expect(req.request.method).toEqual("GET");
+
+    req.flush({ payload: Object.values(COURSES)})
 
   });
 })
