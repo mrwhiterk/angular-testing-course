5c2f91dd3efa19fc22da20377f49a5ba756114e1
Ryan White <mrwhiterk@gmail.com>
2022-09-07 15:43:51 -0400

    20. Angular Test Debugging - How to trigger change detection

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -39,6 +39,13 @@ describe('CoursesCardListComponent', () => {
   it("should display the course list", () => {
 
     component.courses = setupCourses();
+
+    fixture.detectChanges(); // before we can see courses in the DOM. We need to call this to update.
+
+    // console.log(el.nativeElement.outerHTML);
+    
+    
+
     const cards = el.queryAll(By.css(".course-card"));
     expect(cards).toBeTruthy("Could not find cards");
     expect(cards.length).toBe(12, "Unexpected number of courses");
