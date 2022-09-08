62345fcf487bd6642747f21467dda12d3d240828
Ryan White <mrwhiterk@gmail.com>
2022-09-07 21:39:45 -0400

    25. Simulating User DOM Interaction in Angular Unit Tests

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -100,7 +100,21 @@ describe('HomeComponent', () => {
 
   it("should display advanced courses when tab clicked", () => {
 
-    pending();
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css(".mat-tab-label"));
+
+    // el.nativeElement.click();
+
+    click(tabs[1]);
+
+    const cardTitles = el.queryAll(By.css(".mat-card-title"));
+
+    expect(cardTitles.length).toBeGreaterThan(0, "Could not find card titles");
+
+    expect(cardTitles[0].nativeElement.textContent).toContain("Angular Security Course");
 
   });
 
