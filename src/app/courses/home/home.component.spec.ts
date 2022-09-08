38cd7b0f4d5690d49e5a47dac7ed1f36f0df0f9a
Ryan White <mrwhiterk@gmail.com>
2022-09-07 21:21:34 -0400

    24. Container Component Test Suite - The Home Component

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -24,7 +24,10 @@ describe('HomeComponent', () => {
   let coursesService: any;
 
   const beginnerCourses = setupCourses()
-  .filter(course => course.category == 'BEGINNER');
+  .filter(course => course.category == 'BEGINNER'); 
+  
+  const advancedCourses = setupCourses()
+  .filter(course => course.category == 'ADVANCED');
 
   beforeEach(async (() => {
 
@@ -71,14 +74,26 @@ describe('HomeComponent', () => {
 
   it("should display only advanced courses", () => {
 
-      pending();
+    coursesService.findAllCourses.and.returnValue(of(advancedCourses));
+
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css(".mat-tab-label"));
+
+    expect(tabs.length).toBe(1, "Unexpected number of tabs found");
 
   });
 
 
   it("should display both tabs", () => {
 
-    pending();
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css(".mat-tab-label"));
+
+    expect(tabs.length).toBe(2, "Expected to find 2 tabs");
 
   });
 
